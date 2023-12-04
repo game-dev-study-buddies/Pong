71ccd01482a5def99056f4ba7f27d57a71544ba1
brendalongbottom <151066260+brendalongbottom@users.noreply.github.com>
2023-12-04 10:19:00 -0800

    Fix: computer paddle and player paddle (#4)
    * feat: add computer paddle, follows ball, ball var is player paddle
    
    * feat: connect ball variable to Ball instance
    
    * Fix: Move player_paddle and computer_paddle child nodes center to parent node center
    
    Done in 2D scene screen
    
    * Fix: Set ComputerPaddle to origin
    
    ---------
    
    Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com>

--- ball.gd
+++ ball.gd
@@ -1,15 +1,16 @@
 extends RigidBody2D
 
 
-var velocity = Vector2(randi_range(randi_range(150,250),randi_range(150,250)), randi_range(randi_range(150,250),randi_range(150,250)))
+var velocity = Vector2(300,300)
 var rand_direction = randi_range(0,1)
 
 
 func _physics_process(delta):
 	var collision_info
-	if rand_direction == 0:
-		collision_info = move_and_collide(-velocity * delta)
-	else:
-		collision_info = move_and_collide(velocity * delta)
+#	if rand_direction == 0:
+#		collision_info = move_and_collide(-velocity * delta)
+#	else:
+	collision_info = move_and_collide(velocity * delta)
+	
 	if collision_info:
 		velocity = velocity.bounce(collision_info.get_normal())
---
+++ computer_paddle.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+var ball
+
+func _ready():
+	ball = $"../Ball"
+
+func _physics_process(delta):
+	var ai_to_ball = self.global_position.direction_to(ball.global_position)
+	print(
+		"computer: ", snappedf(self.global_position.y, 0.01),
+		" ball: ", snappedf(ball.global_position.y, 0.01),
+		" ai_to_ball: ", snappedf(ai_to_ball.y, 0.01))
+	ai_to_ball.x = 0
+	# round off the ai_to_ball.y so that the paddle doesn't vigorously
+	# shake from top to bottom when the vector's y is close to 0
+	ai_to_ball.y = snappedf(ai_to_ball.y, 0.01)
+
+
+	if ai_to_ball.y > 0:
+		ai_to_ball.y = 1
+	elif ai_to_ball.y < 0:
+		ai_to_ball.y = -1
+		
+	velocity.y = ai_to_ball.y * SPEED 
+
+	move_and_slide()
+	
