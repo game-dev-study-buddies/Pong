37b4a2dc80f78f23b1fb73444c9cca0a54cbd8f8
Christopher Long <93620761+cvalong@users.noreply.github.com>
2023-12-04 11:50:45 -0800

    fix: position score tracking labels, add label built in scripts to gameboard

--- PlayerVariables.gd
+++ PlayerVariables.gd
@@ -1,8 +1,18 @@
 extends Node
 
+var score_one = 0
+var player_one_score: int:
+	get:
+		return score_one
+	set(value):
+		score_one = value
 
-var player_one_score = 0
-var player_two_score = 0
+var score_two = 0
+var player_two_score: int:
+	get:
+		return score_two
+	set(value):
+		score_two = value
 
 
 var current_text = ""
