61bb9793e0ad1c7932ae823231de5f3846baa200
Christopher Long <93620761+cvalong@users.noreply.github.com>
2023-12-05 10:43:03 -0800

    Add initial `score tracking` global vars and signals to loss areas (#6)
    * feat: add labels and global PlayerVariables
    
    * fix: position score tracking labels, add label built in scripts to gameboard
    
    * fix: correct names of all files
    
    * note: for future ball needs by scoring areas

--- PlayerOneLossArea.gd
+++ PlayerOneLossArea.gd
@@ -11,5 +11,15 @@ func _process(delta):
 	pass
 
 
-func _on_area_entered(area):
-	pass # Replace with function body.
+func _on_body_entered(body):
+	if body.name == "Ball":
+		print(body.name, " inside playerOneLossArea")
+	# increment the other player's score
+		PlayerVariables.player_two_score += 1
+	# check if the game is over
+	# despawn ball
+	# check spawn direction
+	# face ball spawn towards the other player
+	# spawn ball
+	else:
+		print(body.name, " inside playerOneLossArea")
--- computer_paddle.gd
+++ computer_paddle.gd
@@ -10,10 +10,10 @@ func _ready():
 
 func _physics_process(delta):
 	var ai_to_ball = self.global_position.direction_to(ball.global_position)
-	print(
-		"computer: ", snappedf(self.global_position.y, 0.01),
-		" ball: ", snappedf(ball.global_position.y, 0.01),
-		" ai_to_ball: ", snappedf(ai_to_ball.y, 0.01))
+#	print(
+#		"computer: ", snappedf(self.global_position.y, 0.01),
+#		" ball: ", snappedf(ball.global_position.y, 0.01),
+#		" ai_to_ball: ", snappedf(ai_to_ball.y, 0.01))
 	ai_to_ball.x = 0
 	# round off the ai_to_ball.y so that the paddle doesn't vigorously
 	# shake from top to bottom when the vector's y is close to 0
