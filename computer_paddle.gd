b4230236b56aa69393005a09e387578214a84617
Christopher Long <93620761+cvalong@users.noreply.github.com>
2023-12-06 11:00:34 -0800

    fix: computer ball detection area and signal

--- computer_paddle.gd
+++ computer_paddle.gd
@@ -5,27 +5,28 @@ const SPEED = 300.0
 
 var ball
 
-func _ready():
-	ball = $"../Ball"
-
 func _physics_process(delta):
-	var ai_to_ball = self.global_position.direction_to(ball.global_position)
-#	print(
-#		"computer: ", snappedf(self.global_position.y, 0.01),
-#		" ball: ", snappedf(ball.global_position.y, 0.01),
-#		" ai_to_ball: ", snappedf(ai_to_ball.y, 0.01))
-	ai_to_ball.x = 0
-	# round off the ai_to_ball.y so that the paddle doesn't vigorously
-	# shake from top to bottom when the vector's y is close to 0
-	ai_to_ball.y = snappedf(ai_to_ball.y, 0.01)
+	if ball:
+		var ai_to_ball = self.global_position.direction_to(ball.global_position)
+	#	print(
+	#		"computer: ", snappedf(self.global_position.y, 0.01),
+	#		" ball: ", snappedf(ball.global_position.y, 0.01),
+	#		" ai_to_ball: ", snappedf(ai_to_ball.y, 0.01))
+		ai_to_ball.x = 0
+		# round off the ai_to_ball.y so that the paddle doesn't vigorously
+		# shake from top to bottom when the vector's y is close to 0
+		ai_to_ball.y = snappedf(ai_to_ball.y, 0.01)
+
+		if ai_to_ball.y > 0:
+			ai_to_ball.y = 1
+		elif ai_to_ball.y < 0:
+			ai_to_ball.y = -1
+
+		velocity.y = ai_to_ball.y * SPEED
+
+		move_and_slide()
 
 
-	if ai_to_ball.y > 0:
-		ai_to_ball.y = 1
-	elif ai_to_ball.y < 0:
-		ai_to_ball.y = -1
-		
-	velocity.y = ai_to_ball.y * SPEED 
-
-	move_and_slide()
-	
+func _on_computer_ball_detection_body_entered(body):
+	if body.name == "Ball":
+		ball = body
