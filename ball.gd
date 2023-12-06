8334f15b40c131fd238502550ab8bbf4dd295e01
brendalongbottom <151066260+brendalongbottom@users.noreply.github.com>
2023-12-06 10:12:14 -0800

    Add spawn_ball function
    Have ball spawn in random directions

--- ball.gd
+++ ball.gd
@@ -1,20 +1,30 @@
 extends RigidBody2D
 
-var ball_direction = Vector2.ONE
+const BALL_SPEED_START = 300
 var ball_speed = 300
+@onready var ball_direction = Vector2.ZERO
 
-var velocity = ball_direction * ball_speed
+
+var velocity
 var rand_direction = randi_range(0,1)
 
+func _ready():
+	spawn_ball()
 
 func _physics_process(delta):
 	
-	var collision_info
-#	if rand_direction == 0:
-#		collision_info = move_and_collide(-velocity * delta)
-#	else:
-	collision_info = move_and_collide(velocity * delta)
+	var collision_info = move_and_collide(velocity * delta)
 	
 	if collision_info:
 		print(collision_info.get_collider().name)
 		velocity = velocity.bounce(collision_info.get_normal())
+
+
+func spawn_ball():
+	ball_speed = BALL_SPEED_START
+	var ball_direction_h = 1
+	if (randf() >= 0.5):
+		ball_direction_h = -1
+	ball_direction = Vector2(ball_direction_h, randf_range(-1,1)).normalized()
+	velocity = ball_direction * ball_speed
+	print(ball_direction, velocity)
