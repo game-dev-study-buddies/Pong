03fc2a57b7e016f8ee5cac2f7bbbac6563106342
brendalongbottom <151066260+brendalongbottom@users.noreply.github.com>
2023-12-07 09:49:38 -0800

    Ball spawn (#9)
    * Fix: Delete canvas layer
    
    add random ball spawn
    
    * Separate ball_direction and ball_speed
    
    * Add get_collider print test
    
    * Add spawn_ball function
    
    Have ball spawn in random directions
    
    * Add ball reset when colliding with score areas
    
    * Spawn new ball when ball resets
    
    * Uncomment computer paddle
    
    add board_center
    
    * remove print statements

---
+++ NewBall.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+@onready var Ball = preload("res://ball.tscn")
+var new_ball
+
+
+func _on_ball_tree_exited():
+	new_ball = Ball.instantiate()
+	var board_center = Vector2(576, 320)
+	new_ball.position = board_center
+	add_child(new_ball)
+	reset_ball()
+
+
+func reset_ball():
+	new_ball.tree_exited.connect(_on_ball_tree_exited)
--- ball.gd
+++ ball.gd
@@ -1,16 +1,36 @@
 extends RigidBody2D
 
+const BALL_SPEED_START = 300
+var ball_speed = 300
+@onready var ball_direction = Vector2.ZERO
 
-var velocity = Vector2(300,300)
-var rand_direction = randi_range(0,1)
 
+var velocity
+
+
+func _ready():
+	spawn_ball()
 
 func _physics_process(delta):
-	var collision_info
-#	if rand_direction == 0:
-#		collision_info = move_and_collide(-velocity * delta)
-#	else:
-	collision_info = move_and_collide(velocity * delta)
+	
+	var collision_info = move_and_collide(velocity * delta)
 	
 	if collision_info:
 		velocity = velocity.bounce(collision_info.get_normal())
+		
+		if collision_info.get_collider().name == "PlayerOneWall":
+			PlayerVariables.player_two_score += 1
+			queue_free()
+		elif collision_info.get_collider().name == "PlayerTwoWall":
+			PlayerVariables.player_one_score += 1
+			queue_free()
+
+
+func spawn_ball():
+	ball_speed = BALL_SPEED_START
+	var ball_direction_h = 1
+	if (randf() >= 0.5):
+		ball_direction_h = -1
+	ball_direction = Vector2(ball_direction_h, randf_range(-1,1)).normalized()
+	velocity = ball_direction * ball_speed
+
