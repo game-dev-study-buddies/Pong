114fb8ccb6e5b0220170d6ec70ed694eaefa8302
Christopher Long <93620761+cvalong@users.noreply.github.com>
2023-12-08 10:39:05 -0800

    fix: adjust files for GDScript naming conventions

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -1,14 +1,12 @@
 extends RigidBody2D
 
-const left = -1
-const right = 1
+const LEFT = -1
+const RIGHT = 1
+const SPEED = 300
+const SPEED_MULTIPLIER = 1.15
 
-var ball_speed = 300
-
-static var ball_direction_h
-
-var speed_multiplier = 1.15
 var velocity
+static var ball_direction_h
 
 func _ready():
 	spawn_ball()
@@ -18,10 +16,10 @@ func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
 	
 	if collision_info:
-		var collider = collision_info.get_collider().name
-		print(collider)
 		velocity = velocity.bounce(collision_info.get_normal())
-		
+
+		var collider = collision_info.get_collider().name
+
 		if collider == "PlayerOneWall":
 			PlayerVariables.player_two_score += 1
 			if PlayerVariables.current_winner:
@@ -35,28 +33,28 @@ func _physics_process(delta):
 			PlayerVariables.current_winner = 1
 			queue_free()
 		elif collider != "WorldBoundary":
-			velocity *= speed_multiplier
+			velocity *= SPEED_MULTIPLIER
 
 
 func spawn_ball():
-	velocity = get_ball_direction() * ball_speed
+	velocity = get_ball_direction() * SPEED
 
 
 func get_ball_direction():
 	if not PlayerVariables.current_winner:
-		ball_direction_h = right if randf() >= 0.5 else left
+		ball_direction_h = RIGHT if randf() >= 0.5 else LEFT
 	elif PlayerVariables.current_winner != PlayerVariables.last_winner:
 		match PlayerVariables.current_winner:
 			1:
-				ball_direction_h = left
+				ball_direction_h = LEFT
 			2:
-				ball_direction_h = right
+				ball_direction_h = RIGHT
 	else:
 		match ball_direction_h:
-			left:
-				ball_direction_h = right
-			right:
-				ball_direction_h = left
+			LEFT:
+				ball_direction_h = RIGHT
+			RIGHT:
+				ball_direction_h = LEFT
 
 	var ball_direction_v = randf_range(-1,1)
 
--- Paddles/computer_paddle.gd
+++ Paddles/computer_paddle.gd
@@ -8,13 +8,8 @@ var ball
 func _physics_process(delta):
 	if ball:
 		var ai_to_ball = self.global_position.direction_to(ball.global_position)
-	#	print(
-	#		"computer: ", snappedf(self.global_position.y, 0.01),
-	#		" ball: ", snappedf(ball.global_position.y, 0.01),
-	#		" ai_to_ball: ", snappedf(ai_to_ball.y, 0.01))
+
 		ai_to_ball.x = 0
-		# round off the ai_to_ball.y so that the paddle doesn't vigorously
-		# shake from top to bottom when the vector's y is close to 0
 		ai_to_ball.y = snappedf(ai_to_ball.y, 0.01)
 
 		if ai_to_ball.y > 0:
